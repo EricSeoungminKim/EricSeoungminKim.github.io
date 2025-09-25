313a842a8df424fc26e5bfc1b0de3ea353c5bdb1
Seoungmin Kim <99856926+EricSeoungminKim@users.noreply.github.com>
2025-09-25 12:11:07 -0700

    chore: set site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: Set this to your final GitHub Pages URL if you want absolute URLs in sitemap, e.g.
-  // site: 'https://<username>.github.io/<repo>',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://ericseoungminkim.github.io',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
